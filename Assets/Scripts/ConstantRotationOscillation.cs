a3d0450a7dbcab0df63f1d14cf292dfe9729ae5b
Johnny Howe <jonathon.o.howe@gmail.com>
2023-07-21 19:15:50 +1200

    Zombe flip

--- Assets/Scripts/ConstantRotationOscillation.cs
+++ Assets/Scripts/ConstantRotationOscillation.cs
@@ -6,11 +6,18 @@ public class ConstantRotationOscillation : MonoBehaviour
 {
     [SerializeField] private float _maxAngle = 90f;
     [SerializeField] private float _period = 1f;
+    [SerializeField] private bool _randomStart = true;
+    private float _offset = 0;
+
+    private void Start()
+    {
+        if (_randomStart) _offset = _period * Random.Range(0f, 1000f) / 1000f;
+    }
 
     private void Update()
     {
         if (_period == 0) return;
-        float t = Mathf.Sin(2 * Mathf.PI * Time.time / _period);
+        float t = Mathf.Sin(2 * Mathf.PI * (Time.time + _offset) / _period);
         transform.localRotation = Quaternion.Euler(0, 0, t * _maxAngle);
     }
 }
--- Assets/Scripts/ConstantSquashStretch.cs
+++ Assets/Scripts/ConstantSquashStretch.cs
@@ -7,11 +7,18 @@ public class ConstantSquashStretch : MonoBehaviour
     [SerializeField] private Vector3 _squashScale = new Vector3(1.1f, 0.9f, 1);
     [SerializeField] private Vector3 _stretchScale = new Vector3(0.9f, 1.1f, 1);
     [SerializeField] private float _period = 1f;
+    [SerializeField] private bool _randomStart = true;
+    private float _offset = 0;
+
+    private void Start()
+    {
+        if (_randomStart) _offset = _period * Random.Range(0f, 1000f) / 1000f;
+    }
 
     private void Update()
     {
         if (_period == 0) return;
-        float t = Mathf.Sin(2 * Mathf.PI * Time.time / _period) * 0.5f + 0.5f;
+        float t = Mathf.Sin(2 * Mathf.PI * (Time.time + _offset) / _period) * 0.5f + 0.5f;
         transform.localScale = Vector3.Lerp(_squashScale, _stretchScale, t);
     }
 }
